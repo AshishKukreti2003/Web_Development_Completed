24667e4dfafdd84b09ff91f7cf07bdd6c8753e45
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-08-14 21:48:44 +0530

    Add new function

--- .JS-projects/rock-paper-scissors-game/script.js
+++ .JS-projects/rock-paper-scissors-game/script.js
@@ -6,6 +6,56 @@ const score = JSON.parse(localStorage.getItem('score')) || {
 
 updateScoreElement();
 
+let intervalId;
+let isAutoPlaying = false;
+function autoPlay(){
+    if(!isAutoPlaying){
+        intervalId = setInterval(() => {
+            const playerMove = pickComputerMove();
+            playGame(playerMove);
+        },1000);
+        isAutoPlaying = true;
+        document.querySelector('.js-auto-play-button').innerHTML = 'Stop Playing';
+    }else{
+        clearInterval(intervalId);
+        isAutoPlaying = false;
+        document.querySelector('.js-auto-play-button').innerHTML = 'Auto Play';
+    }
+}
+
+document.querySelector('.js-auto-play-button').addEventListener('click',()=>{
+    autoPlay();
+})
+document.querySelector('.js-rock-button').addEventListener('click',()=>{
+    playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click',()=>{
+    playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click',()=>{
+    playGame('scissors');   
+});
+
+document.body.addEventListener('keydown',(event) => {
+    if(event.key === 'r'){
+        playGame('rock');
+    }else if(event.key === 'p'){
+        playGame('paper');
+    }else if(event.key === 's'){
+        playGame('scissors');
+    }
+    
+    else if(event.key === 'a'){
+        autoPlay();
+    }else if(event.key === 'Backspace'){
+        // resetScore();
+        showResetConfirmation(); 
+    }
+});
+
+
 function playGame(playerMove){
     const ComputerMove = pickComputerMove();
     let result = '';
@@ -59,6 +109,8 @@ function updateScoreElement(){
     document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
 }
 
+
+
 function pickComputerMove(){
     const randomNumber = Math.random();
     let ComputerMove = ''; 
@@ -66,8 +118,42 @@ function pickComputerMove(){
         ComputerMove = 'rock';
     }else if(randomNumber >=1/3 && randomNumber < 2/3){
         ComputerMove = 'paper';
-    }else if(randomNumber >= 2/3 &&     randomNumber < 1){
+    }else if(randomNumber >= 2/3 && randomNumber < 1){
         ComputerMove = 'scissors';
     }
     return ComputerMove;
+}
+
+function resetScore(){
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score');
+    updateScoreElement();
+}
+
+document.querySelector('.js-reset-score-button').addEventListener('click',()=>{
+    // resetScore();
+    showResetConfirmation();
+})
+
+function showResetConfirmation(){
+    document.querySelector('.js-reset-confirmation').innerHTML = `
+        Are you sure you want to reset the score?
+        <button class = "js-reset-confirm-yes">Yes</button>
+        <button class = "js-reset-confirm-no">No</button>
+    `;
+
+    document.querySelector('.js-reset-confirm-yes').addEventListener('click',()=>{
+        resetScore();
+        hideResetConfirmation();
+    });
+
+    document.querySelector('.js-reset-confirm-no').addEventListener('click',()=>{
+        hideResetConfirmation();
+    })
+}
+
+function hideResetConfirmation(){
+    document.querySelector('.js-reset-confirmation').innerHTML = '';
 }
